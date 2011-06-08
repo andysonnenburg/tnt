348795355d15933e6b305000bf84a5a4e3af0f11
Andy Sonnenburg <andrew.sonnenburg@InfiniteCampus.com>
2011-06-08 14:49:02 -0500

    major performance improvement via data instead of newtype

--- Control/Monad/Code.hs
+++ Control/Monad/Code.hs
@@ -22,8 +22,8 @@ module Control.Monad.Code
        , execCodeT
        ) where
 
-import Control.Applicative hiding (Const)
-import Control.Monad hiding (return)
+import Control.Applicative
+import Control.Monad
 import Control.Monad.Code.Class hiding (Int (..), return)
 import qualified Control.Monad.Code.Class
 import qualified Control.Monad.Code.Opcode as Opcode
@@ -126,9 +126,6 @@ initState = S { stack = 0
 instance Monad m => Indexed.Monad (CodeT s m) where
   m >>= k = CodeT $ unCodeT m >>= unCodeT . k
   {-# INLINE (>>=) #-}
-
-  m >> n = CodeT $ unCodeT m >> unCodeT n
-  {-# INLINE (>>) #-}
   
   return = CodeT . return
   {-# INLINE return #-}
@@ -137,24 +134,24 @@ instance Monad m => Indexed.Monad (CodeT s m) where
 
 instance MonadConstantPool m => MonadCode (CodeT s m) where
   
-  newtype Label (CodeT s m) xs = Label Int32
+  data Label (CodeT s m) xs = Label Int32
 
   aaload = insn (-1) Opcode.aaload
   aastore = insn (-3) Opcode.aastore
   aconst_null = insn 1 Opcode.aconst_null
 
   aload var = case var of
-    0 -> insn' 1 1 1 $! putWord8 Opcode.aload_0
-    1 -> insn' 1 2 1 $! putWord8 Opcode.aload_1
-    2 -> insn' 1 3 1 $! putWord8 Opcode.aload_2
-    3 -> insn' 1 4 1 $! putWord8 Opcode.aload_3
+    0 -> insn' 1 1 1 $ putWord8 Opcode.aload_0
+    1 -> insn' 1 2 1 $ putWord8 Opcode.aload_1
+    2 -> insn' 1 3 1 $ putWord8 Opcode.aload_2
+    3 -> insn' 1 4 1 $ putWord8 Opcode.aload_3
     _ -> varInsn 1 var Opcode.aload
 
   astore var = case var of
-    0 -> insn' (-1) 1 1 $! putWord8 Opcode.astore_0
-    1 -> insn' (-1) 2 1 $! putWord8 Opcode.astore_1
-    2 -> insn' (-1) 3 1 $! putWord8 Opcode.astore_2
-    3 -> insn' (-1) 4 1 $! putWord8 Opcode.astore_3
+    0 -> insn' (-1) 1 1 $ putWord8 Opcode.astore_0
+    1 -> insn' (-1) 2 1 $ putWord8 Opcode.astore_1
+    2 -> insn' (-1) 3 1 $ putWord8 Opcode.astore_2
+    3 -> insn' (-1) 4 1 $ putWord8 Opcode.astore_3
     _ -> varInsn (-1) var Opcode.astore
 
   baload = insn (-1) Opcode.baload
@@ -165,101 +162,101 @@ instance MonadConstantPool m => MonadCode (CodeT s m) where
 
   dup2 = insn 2 Opcode.dup2
 
-  getstatic typ name fieldType = CodeT $! do
-    x <- lift $! lookupField typ name (desc fieldType)
-    unCodeT $! insn' (stackSize fieldType) 0 3 $! do
+  getstatic typ name fieldType = CodeT $ do
+    x <- lift $ lookupField typ name (desc fieldType)
+    unCodeT $ insn' (stackSize fieldType) 0 3 $ do
       putWord8 Opcode.getstatic
       putWord16be x
 
-  goto (Label target) = CodeT $! do
+  goto (Label target) = CodeT $ do
     s@S {..} <- get
     let offset = target - fromIntegral codeLength
     when (offset > fromIntegral (maxBound :: Int16) ||
-          offset < fromIntegral (minBound :: Int16)) $!
+          offset < fromIntegral (minBound :: Int16)) $
       fail "TODO: wide branch offsets"
     put s { codeLength = codeLength + 3
           , code = code >> do
             putWord8 Opcode.goto
-            putWord16be $! fromIntegral $! offset
+            putWord16be $! fromIntegral offset
           }
-    return $! Label . fromIntegral $! codeLength
+    return . Label . fromIntegral $ codeLength
 
   i2b = insn 0 Opcode.i2b
   
   iadd = insn (-1) Opcode.iadd
 
-  ifeq (Label target) = CodeT $! do
+  ifeq (Label target) = CodeT $ do
     s@S {..} <- get
     let offset = target - fromIntegral codeLength
     when (target > fromIntegral (maxBound :: Int16) ||
-          target < fromIntegral (minBound :: Int16)) $!
+          target < fromIntegral (minBound :: Int16)) $
       fail "TODO: wide branch offsets"
-    put $! s { stack = stack - 1
-            , codeLength = codeLength + 3
-            , code = code >> do
-              putWord8 Opcode.ifeq
-              putWord16be $! fromIntegral $! offset
-            }
-    return $! Label . fromIntegral $! codeLength
+    put s { stack = stack - 1
+          , codeLength = codeLength + 3
+          , code = code >> do
+            putWord8 Opcode.ifeq
+            putWord16be $! fromIntegral offset
+          }
+    return . Label . fromIntegral $ codeLength
   
   iinc local cnst
     | local <= fromIntegral (maxBound :: Word8) &&
       cnst >= fromIntegral (minBound :: Int8) &&
       cnst <= fromIntegral (maxBound :: Int8) =
-      insn' 0 local 3 $! do
+      insn' 0 local 3 $ do
         putWord8 Opcode.iinc
-        putWord8 $! fromIntegral $! local
-        putWord8 $! fromIntegral $! cnst
+        putWord8 $! fromIntegral local
+        putWord8 $! fromIntegral cnst
     | cnst >= fromIntegral (minBound :: Int16) &&
       cnst <= fromIntegral (maxBound :: Int16) =
-      insn' 0 local 6 $! do
+      insn' 0 local 6 $ do
         putWord8 Opcode.wide
         putWord8 Opcode.iinc
         putWord16be local
-        putWord16be $! fromIntegral $! cnst
+        putWord16be $! fromIntegral cnst
     | otherwise = iload local Indexed.>>= \label ->
                   ldc cnst Indexed.>>
                   iadd Indexed.>>
                   istore local Indexed.>>
-                  return label
+                  Indexed.return label
 
   iload j = case j of
-    0 -> insn' 1 1 1 $! putWord8 Opcode.iload_0
-    1 -> insn' 1 2 1 $! putWord8 Opcode.iload_1
-    2 -> insn' 1 3 1 $! putWord8 Opcode.iload_2
-    3 -> insn' 1 4 1 $! putWord8 Opcode.iload_3
+    0 -> insn' 1 1 1 $ putWord8 Opcode.iload_0
+    1 -> insn' 1 2 1 $ putWord8 Opcode.iload_1
+    2 -> insn' 1 3 1 $ putWord8 Opcode.iload_2
+    3 -> insn' 1 4 1 $ putWord8 Opcode.iload_3
     _ -> varInsn 1 j Opcode.iload
 
   istore var = case var of
-    0 -> insn' (-1) 1 1 $! putWord8 Opcode.istore_0
-    1 -> insn' (-1) 2 1 $! putWord8 Opcode.istore_1
-    2 -> insn' (-1) 3 1 $! putWord8 Opcode.istore_2
-    3 -> insn' (-1) 4 1 $! putWord8 Opcode.istore_3
+    0 -> insn' (-1) 1 1 $ putWord8 Opcode.istore_0
+    1 -> insn' (-1) 2 1 $ putWord8 Opcode.istore_1
+    2 -> insn' (-1) 3 1 $ putWord8 Opcode.istore_2
+    3 -> insn' (-1) 4 1 $ putWord8 Opcode.istore_3
     _ -> varInsn (-1) var Opcode.istore
 
-  invokeinterface typ method args result = CodeT $! do
-    x <- lift $! lookupInterfaceMethod typ method dsc
-    unCodeT $! insn' i 0 5 $! do
+  invokeinterface typ method args result = CodeT $ do
+    x <- lift $ lookupInterfaceMethod typ method dsc
+    unCodeT $ insn' i 0 5 $ do
       putWord8 Opcode.invokeinterface
       putWord16be x
-      putWord8 $! (+ 1) . fromIntegral . stackSize $! args
+      putWord8 $! (+ 1) . fromIntegral . stackSize $ args
       putWord8 0
     where
       dsc = methodDesc args result
       i = stackSize result - stackSize args
 
-  invokespecial typ method args result = CodeT $! do
-    x <- lift $! lookupMethod typ method dsc
-    unCodeT $! insn' i 0 3 $! do
+  invokespecial typ method args result = CodeT $ do
+    x <- lift $ lookupMethod typ method dsc
+    unCodeT $ insn' i 0 3 $ do
       putWord8 Opcode.invokespecial
       putWord16be x
     where
       dsc = methodDesc args result
       i = stackSize result - stackSize args
 
-  invokevirtual typ method args result = CodeT $! do
-    x <- lift $! lookupMethod typ method dsc
-    unCodeT $! insn' i 0 3 $! do
+  invokevirtual typ method args result = CodeT $ do
+    x <- lift $ lookupMethod typ method dsc
+    unCodeT $ insn' i 0 3 $ do
       putWord8 Opcode.invokevirtual
       putWord16be x
     where
@@ -278,13 +275,13 @@ instance MonadConstantPool m => MonadCode (CodeT s m) where
     5 -> insn 1 Opcode.iconst_5
     _
       | x >= fromIntegral (minBound :: Int8) &&
-        x <= fromIntegral (maxBound :: Int8) -> insn' 1 0 2 $! do
+        x <= fromIntegral (maxBound :: Int8) -> insn' 1 0 2 $ do
           putWord8 Opcode.bipush
-          putWord8 $! fromIntegral $! x
+          putWord8 $! fromIntegral x
       | x >= fromIntegral (minBound :: Int16) &&
-        x <= fromIntegral (maxBound :: Int16) -> insn' 1 0 3 $! do
+        x <= fromIntegral (maxBound :: Int16) -> insn' 1 0 3 $ do
           putWord8 Opcode.sipush
-          putWord16be $! fromIntegral $! x
+          putWord16be $! fromIntegral x
       | otherwise -> ldcInsn lookupInteger x
 
   ldcFloat x = case x of
@@ -307,9 +304,9 @@ instance MonadConstantPool m => MonadCode (CodeT s m) where
     1 -> insn 2 Opcode.dconst_1
     _ -> ldc2Insn lookupDouble x
 
-  new typ = CodeT $! do
-    x <- lift $! lookupClass typ
-    unCodeT $! insn' 1 0 3 $ do
+  new typ = CodeT $ do
+    x <- lift $ lookupClass typ
+    unCodeT $ insn' 1 0 3 $ do
       putWord8 Opcode.new
       putWord16be x
 
@@ -325,7 +322,7 @@ instance MonadConstantPool m => MonadCode (CodeT s m) where
       T_INT -> 10
       T_LONG -> 11
 
-  nop = CodeT $! liftM (Label . fromIntegral . codeLength) get
+  nop = CodeT $ liftM (Label . fromIntegral . codeLength) get
 
   return = insn 0 Opcode.return
 
@@ -335,20 +332,20 @@ insn' :: Monad m =>
          Word16 ->
          Put ->
          CodeT s m i j (Label (CodeT s m) i)
-insn' i j n m = CodeT $! do
+insn' i j n m = CodeT $ do
   S {..} <- get
   let stack' = stack + i
-  put $! S { stack = stack'
-           , maxStack = max maxStack stack'
-           , maxLocals = max maxLocals j
-           , codeLength = codeLength + n
-           , code = code >> m
-           }
-  return $! Label . fromIntegral $! codeLength
+  put S { stack = stack'
+               , maxStack = max maxStack stack'
+               , maxLocals = max maxLocals j
+               , codeLength = codeLength + n
+               , code = code >> m
+               }
+  return . Label . fromIntegral $ codeLength
 {-# INLINE insn' #-}
 
 insn :: Monad m => Word16 -> Word8 -> CodeT s m i j (Label (CodeT s m) i)
-insn i opcode = insn' i 0 1 $! putWord8 opcode
+insn i opcode = insn' i 0 1 $ putWord8 opcode
 {-# INLINE insn #-}
 
 varInsn :: Monad m =>
@@ -357,10 +354,10 @@ varInsn :: Monad m =>
            Word8 ->
            CodeT s m i j (Label (CodeT s m) i)
 varInsn i var opcode
-  | var < fromIntegral (maxBound :: Word8) = insn' i (var + 1) 2 $! do
+  | var < fromIntegral (maxBound :: Word8) = insn' i (var + 1) 2 $ do
     putWord8 opcode
-    putWord8 $! fromIntegral $! var
-  | otherwise = insn' i (var + 1) 3 $! do
+    putWord8 $! fromIntegral var
+  | otherwise = insn' i (var + 1) 3 $ do
     putWord8 Opcode.wide
     putWord8 opcode
     putWord16be var
@@ -370,7 +367,7 @@ ldcInsn :: MonadConstantPool m =>
            a ->
            CodeT s m i j (Label (CodeT s m) i)
 ldcInsn k x = CodeT $ do
-  x' <- lift $! k x
+  x' <- lift $ k x
   let (n, p, o) = if x' < fromIntegral (maxBound :: Word8)
                   then (2, putWord8 . fromIntegral, Opcode.ldc)
                   else (3, putWord16be, Opcode.ldc_w)
@@ -383,7 +380,7 @@ ldc2Insn :: MonadConstantPool m =>
             a ->
             CodeT s m i j (Label (CodeT s m) i)
 ldc2Insn k x = CodeT $ do
-  x' <- lift $! k x
+  x' <- lift $ k x
   unCodeT $ insn' 2 0 3 $ do
     putWord8 Opcode.ldc2_w
     putWord16be x'
--- Language/Brainfuck/Emitter.hs
+++ Language/Brainfuck/Emitter.hs
@@ -1,27 +1,31 @@
 {-# LANGUAGE DoRec, NoMonomorphismRestriction, RebindableSyntax #-}
+{-# LANGUAGE RankNTypes #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing -fno-warn-unused-do-bind #-}
 module Language.Brainfuck.Emitter (emit) where
 
 import Control.Monad (forM_)
 import Control.Monad.Fix
-import Control.Monad.Code.Class
+import Control.Monad.Code
 import Control.Monad.Indexed hiding (return)
 import qualified Control.Monad.Indexed as M
+
+import Data.Int
 
 import Language.Brainfuck.Command
 
 import Prelude hiding (Monad (..))
 
-emit :: ( MonadCode m
-        , MonadFix (m () ())
-        ) => [Command] -> m () () (Label m ())
+type Emit i = forall m. (MonadFix (m i i), MonadCode m) => m i i (Label m i)
+-- type Emit i = forall s. Code s i i (Label (Code s) i)
+
+emit :: [Command] -> Emit ()
 emit xs = do
   label <- emitHeader
   forM_ xs emitCommand
   emitFooter
   M.return label
 
-emitHeader :: MonadCode m => m i i (Label m i)
+emitHeader :: Emit i
 emitHeader = do
   label <- ldc 30000
   newarray T_BYTE
@@ -30,9 +34,18 @@ emitHeader = do
   istore 2
   M.return label
 
-emitCommand :: (MonadCode m, MonadFix (m i i)) => Command -> m i i (Label m i)
-emitCommand (IncrementPointer x) = iinc 2 x
-emitCommand (IncrementByte x) = do
+emitCommand :: Command -> Emit i
+emitCommand (IncrementPointer x) = emitIncrementPointer x
+emitCommand (IncrementByte x) = emitIncrementByte x
+emitCommand OutputByte = emitOutputByte
+emitCommand InputByte = emitInputByte
+emitCommand (WhileNonzero xs) = emitWhileNonzero xs
+
+emitIncrementPointer :: Int32 -> Emit i
+emitIncrementPointer = iinc 2
+
+emitIncrementByte :: Int32 -> Emit i
+emitIncrementByte x = do
   label <- aload 1
   iload 2
   dup2
@@ -42,14 +55,18 @@ emitCommand (IncrementByte x) = do
   i2b
   bastore
   M.return label
-emitCommand OutputByte = do
+
+emitOutputByte :: Emit i
+emitOutputByte = do
   label <- getstatic "java/lang/System" "out" (L"java/io/PrintStream")
   aload 1
   iload 2
   baload
   invokevirtual "java/io/PrintStream" "write" (I)V
   M.return label
-emitCommand InputByte = do
+
+emitInputByte :: Emit i
+emitInputByte = do
   label <- aload 1
   iload 2
   getstatic "java/lang/System" "in" (L"java/io/InputStream")
@@ -57,7 +74,9 @@ emitCommand InputByte = do
   i2b
   bastore
   M.return label
-emitCommand (WhileNonzero xs) = do
+
+emitWhileNonzero :: [Command] -> Emit i
+emitWhileNonzero xs = do
   (start, _) <- mfix (\ ~(_, end) -> do
     start <- aload 1
     iload 2
@@ -68,19 +87,8 @@ emitCommand (WhileNonzero xs) = do
     end <- nop
     M.return (start, end))
   M.return start
-{- emitCommand (WhileNonzero xs) =
-  do
-    rec
-      start <- aload 1
-      iload 2
-      baload
-      ifeq end
-      forM_ xs emitCommand
-      goto start
-      end <- nop
-    M.return start -}
 
-emitFooter :: MonadCode m => m i i (Label m i)
+emitFooter :: Emit i
 emitFooter = do
   getstatic "java/lang/System" "out" (L"java/io/PrintStream")
   invokevirtual "java/io/PrintStream" "flush" ()V
